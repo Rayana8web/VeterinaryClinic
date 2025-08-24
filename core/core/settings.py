45f852f82aa9d0281a3e454e9de8146a44c6f7ab
Rayana <raana7379@gmail.com>
2025-08-24 20:16:30 +0600

    CORS_ALLOW_ALL_ORIGINS

--- core/core/settings.py
+++ core/core/settings.py
@@ -143,8 +143,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5175",
-    "https://web08hg.pythonanywhere.com",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
