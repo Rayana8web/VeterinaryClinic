cd5c1143b561a02674417e5e4ca3e1005d0d6a6e
Rayana <raana7379@gmail.com>
2025-08-29 06:38:53 +0600

    cors

--- core/core/settings.py
+++ core/core/settings.py
@@ -18,9 +18,9 @@ from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend_dist")]
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -61,16 +61,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # <-- теперь это корень VeterinaryСlinic
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend_dist")]
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "frontend" / "dist"],  # путь к dist
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -101,8 +106,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "core" / "static"]  # локальные статики
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+ # локальные статики
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = 'media/'
